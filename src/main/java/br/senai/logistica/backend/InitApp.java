692efe7cab10e93545720f7f5dc6c2c6b6492e03
Yago Matheus <yagofx@hotmail.com>
2022-07-07 21:47:20 -0300

    Criado esqueleto do projeto

---
+++ src/main/java/br/senai/logistica/backend/InitApp.java
@@ -0,0 +1,13 @@
+package br.senai.logistica.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitApp.class, args);
+	}
+
+}
