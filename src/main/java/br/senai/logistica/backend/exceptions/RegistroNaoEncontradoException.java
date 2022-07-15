d8fea42fa8f97a5b696054ad5e986821eff1ce33
Yago Matheus <yagofx@hotmail.com>
2022-07-14 21:41:06 -0300

    Adicionado tratamento de erros nos Controllers

---
+++ src/main/java/br/senai/logistica/backend/exceptions/RegistroNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.senai.logistica.backend.exceptions;
+
+public class RegistroNaoEncontradoException extends RuntimeException {
+
+	private static final long serialVersionUID = 2469420101L;
+
+}
