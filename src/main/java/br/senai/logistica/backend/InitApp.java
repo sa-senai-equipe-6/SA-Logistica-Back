152e09414039c1c9fb7be32674b34a91c516ef49
Yago Matheus <yagofx@hotmail.com>
2022-07-14 21:40:53 -0300

    Refatorações gerais

--- src/main/java/br/senai/logistica/backend/InitApp.java
+++ src/main/java/br/senai/logistica/backend/InitApp.java
@@ -1,18 +1,13 @@
 package br.senai.logistica.backend;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InitApp {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(InitApp.class, args);
-		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		System.out.println(LocalDate.now().format(formatter));
+		SpringApplication.run(InitApp.class, args);
 	}
 
 }
