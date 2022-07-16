5aa539114494694cf3258de606a47a71ef50345a
Yago Matheus <yagofx@hotmail.com>
2022-07-15 21:41:55 -0300

    Configurado Service e Repository de MeioTransporte

---
+++ src/main/java/br/senai/logistica/backend/repository/MeioTransporteRepository.java
@@ -0,0 +1,9 @@
+package br.senai.logistica.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.senai.logistica.backend.entity.MeioTransporte;
+
+public interface MeioTransporteRepository extends JpaRepository<MeioTransporte, Integer>{
+
+}
---
+++ src/main/java/br/senai/logistica/backend/service/MeioTransporteService.java
@@ -0,0 +1,53 @@
+package br.senai.logistica.backend.service;
+
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import com.google.common.base.Preconditions;
+
+import br.senai.logistica.backend.entity.MeioTransporte;
+import br.senai.logistica.backend.repository.MeioTransporteRepository;
+
+@Service
+@Validated
+public class MeioTransporteService {
+	
+	@Autowired
+	private MeioTransporteRepository repository;
+	
+	public MeioTransporte inserir(
+			@Valid
+			@NotNull(message = "O meio de transporte é obrigatorio")
+			MeioTransporte transporteRecebido) {
+		
+		Preconditions.checkArgument(transporteRecebido.getId() == null, "O id do meio de transporte deve ser nulo");
+		return repository.save(transporteRecebido);
+	}
+	
+	public MeioTransporte alterar(
+			@Valid
+			@NotNull(message = "O meio de transporte é obrigatorio")
+			MeioTransporte transporteRecebido) {
+		
+		Preconditions.checkArgument(transporteRecebido.getId() != null, "O id do meio de transporte é obrigatorio");
+		return repository.save(transporteRecebido);
+	}
+	
+	public void deletarPor(
+			@Valid
+			@NotNull(message = "O id do transporte é obrigatorio")
+			Integer id) {
+		repository.deleteById(id);
+	}
+	
+	public List<MeioTransporte> listarTodos() {
+		return repository.findAll();
+	}
+	
+}
