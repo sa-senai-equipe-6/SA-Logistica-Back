8fa828dd17d21dbbfdbd7157a9305ce79d9e4b04
Yago Matheus <yagofx@hotmail.com>
2022-07-29 21:08:48 -0300

    Adicionado função deletar em MotoristaController

--- src/main/java/br/senai/logistica/backend/controller/MotoristaController.java
+++ src/main/java/br/senai/logistica/backend/controller/MotoristaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -37,6 +38,15 @@ public class MotoristaController {
 		service.editar(motorista);
 		return ResponseEntity.ok().build();
 	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> deletarMotorista(
+			@RequestBody
+			Motorista motorista) {
+		service.deletarPor(motorista.getId());
+		return ResponseEntity.ok().build();
+	}
+			
 	
 	@GetMapping
 	public ResponseEntity<?> listarTodos() {
